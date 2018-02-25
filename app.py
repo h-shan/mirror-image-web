7fd083e72243bbacfd2370dd1d9d5214ccf30db8
h-shan <howardshan11@gmail.com>
2018-02-24 21:37:00 -0600

    Try hitting google api

--- app.py
+++ app.py
@@ -16,12 +16,13 @@ def chat():
 @socketio.on('send message')
 def send_message(message_text):
     message = message_text['data']
-    res = requests.post('https://mirror-image.herokuapp.com', \
+    res = requests.post('https://google.com', \
             json={'message': message}).content.decode()
+    print('google ', res)
     if res:
         # messages.append(['chatbot', 'CPU', r, datetime.now().strftime('%h. %d, %Y. %I:%M:%S %p')])
         message_time = datetime.now().strftime('%m/%d/%Y %I:%M:%S %p')
-        socketio.emit('server', {'message': res, 'sender': 'CPU', "class": "chatbot", "time": message_time}) 
+        socketio.emit('server', {'message': res, 'sender': 'CPU', 'class': 'chatbot', 'time': message_time}) 
 
 if __name__ == '__main__':
     socketio.run(app)
